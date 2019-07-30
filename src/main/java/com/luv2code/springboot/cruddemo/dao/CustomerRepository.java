da5bc3cd38f256905f84ca46fdb2bc5d6f005741
Mateusz Baranski <mateusz.baranski@ericsson.com>
2019-07-30 15:31:20 +0200

    some chane

--- src/main/java/com/luv2code/springboot/cruddemo/dao/CustomerRepository.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.luv2code.springboot.cruddemo.dao;
 
 import com.luv2code.springboot.cruddemo.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface CustomerRepository  extends JpaRepository<Customer,Integer> {
 }
